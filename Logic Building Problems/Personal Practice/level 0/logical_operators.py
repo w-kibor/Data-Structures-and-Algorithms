4bb9a2caaa3d336ed38f74c2e61162e281b6f13e
w-kibor <kiborwilkister29@gmail.com>
2026-02-16 23:57:41 +0300

    or

--- Logic Building Problems/Personal Practice/level 0/logical_operators.py
+++ Logic Building Problems/Personal Practice/level 0/logical_operators.py
@@ -14,3 +14,18 @@ has_ticket=True
 #check the logic or the condition
 if age >= 18 and has_ticket:
     print("You can enter the concert.")
+else:    print("You cannot enter the concert.")
+
+#or operator: returns True if at least one condition is true
+#The example problem we will solve is this imagine getting into a concert were we will have to satisfy at least one condition
+#Condition 1: You need to be 18 or above to enter the concert
+#Condition 2: You need to have a ticket to enter the concert
+
+#setting the variables
+age=18
+has_ticket=False
+
+#check the logic or the condition
+if age >= 18 or has_ticket:
+    print("You can enter the concert.")
+else:    print("You cannot enter the concert.")

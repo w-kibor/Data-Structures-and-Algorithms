ea567752d78d6687024914f916441ae9dd058de8
w-kibor <kiborwilkister29@gmail.com>
2026-02-16 23:38:29 +0300

    elif

---
+++ Logic Building Problems/Personal Practice/level 0/elif.py
@@ -0,0 +1,12 @@
+#using an example of temperature i want to know if it is hot nice or freezing
+
+#setting the variable
+temperature=10
+
+#check the logic or the condition
+if temperature < 10:
+    print("It's a freezing day.")
+elif temperature > 30:
+    print("It's a hot day.")
+else:
+    print("The weather is nice.")

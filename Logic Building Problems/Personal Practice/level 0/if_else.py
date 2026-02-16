75600acbbdf0dc93dc46fad4ead60701a9e4f89a
w-kibor <kiborwilkister29@gmail.com>
2026-02-16 23:30:37 +0300

    understanding logic

---
+++ Logic Building Problems/Personal Practice/level 0/if_else.py
@@ -0,0 +1,20 @@
+# For example if a kid needds to be above 18 to access a website, you can use an if statement to check if the kid's age is above 18. If it is, you can allow them to access the website, otherwise you can deny access.
+
+#setting the variable
+age=18
+
+#check the logic or the condition
+if age >= 18:
+    print("You can access the website.")
+else:    print("You cannot access the website.")
+
+#Maybe another example could be checking whether the temperature is 30 degrees or above
+
+#setting the variable
+temperature=30
+
+#check the logic or the condition
+if temperature >= 30:
+    print("It's a hot day.")    
+else:    print("Favorable temperature for humans.")
+

1fde0d79554f7603d5f09d9219f86446507b3e74
Wilkister Kibor <kiborwilkister29@gmail.com>
2026-03-24 23:38:23 +0300

    solution using amathematics logic

--- Logic Building Problems/Personal Practice/level 0/palindrome.py
+++ Logic Building Problems/Personal Practice/level 0/palindrome.py
@@ -21,3 +21,24 @@ class Solution:
         return s==s[::-1] #type: ignore
 
 print(Solution().isPalindrome(121))
+
+#using mathematics logic
+#we willuse modulo %10 to find the last digit and //10 to remove the last digit
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        #negative numbers are not palindromes
+        if x<0:
+            return False
+        original=x
+        reversed_num=0
+        while x>0:
+            #get the last digit
+            last_digit=x%10
+            #reverse the number
+            reversed_num=reversed_num*10+last_digit
+            #remove the last digit
+            x=x//10
+        #compare the original and reversed number
+        return original==reversed_num
+
